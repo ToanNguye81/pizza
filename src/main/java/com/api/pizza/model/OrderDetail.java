fa7d4635965fab2ca3cb820f6ee229c1f9f0720f
Your Name <n.t.quoctoan@gmail.com>
2023-07-05 20:05:26 +0700

    add orderDetail

---
+++ src/main/java/com/api/pizza/model/OrderDetail.java
@@ -0,0 +1,97 @@
+package com.api.pizza.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "orders")
+public class OrderDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private int id;
+
+    @Column(name = "quantity_order")
+    private int quantityOrder;
+
+    @Column(name = "price_each")
+    private BigDecimal priceEach;
+
+    @ManyToOne
+    @JsonIgnore
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_date", nullable = true, updatable = false)
+    @CreatedDate
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_date", nullable = true)
+    @LastModifiedDate
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private Date updatedDate;
+
+    public OrderDetail() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public void setPriceEach(BigDecimal priceEach) {
+        this.priceEach = priceEach;
+    }
+
+    public void setQuantityOrder(int quantityOrder) {
+        this.quantityOrder = quantityOrder;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public BigDecimal getPriceEach() {
+        return priceEach;
+    }
+
+    public int getQuantityOrder() {
+        return quantityOrder;
+    }
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+}
