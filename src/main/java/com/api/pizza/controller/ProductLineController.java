fd1b51796ce5cd186ba23376141cd5b46b5324b9
Your Name <n.t.quoctoan@gmail.com>
2023-07-06 23:19:48 +0700

    add productLine

--- src/main/java/com/api/pizza/controller/ProductLineController.java
+++ src/main/java/com/api/pizza/controller/ProductLineController.java
@@ -1,5 +1,6 @@
 package com.api.pizza.controller;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,9 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.api.pizza.model.Customer;
 import com.api.pizza.model.ProductLine;
-import com.api.pizza.repository.ICustomerRepository;
 import com.api.pizza.repository.IProductLineRepository;
 
 @RestController
@@ -33,11 +32,9 @@ import com.api.pizza.repository.IProductLineRepository;
 public class ProductLineController {
     @Autowired
     IProductLineRepository gProductLineRepository;
-    @Autowired
-    ICustomerRepository gCustomerRepository;
 
     // get all ProductLine
-    @GetMapping("/product-lines")
+    @GetMapping("/customers")
     public ResponseEntity<List<ProductLine>> getAllProductLine(
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size", defaultValue = "10") int size) {
@@ -56,10 +53,33 @@ public class ProductLineController {
         }
     }
 
-    // get payment by id
-    @GetMapping("product-lines/{paymentId}")
-    public ResponseEntity<Object> getProductLineById(@PathVariable Integer paymentId) {
-        Optional<ProductLine> vProductLineData = gProductLineRepository.findById(paymentId);
+    // create new customer
+    @PostMapping("/customers")
+    public ResponseEntity<Object> createNewProductLine(
+            @Valid @RequestBody ProductLine pProductLine) {
+        try {
+            ProductLine vProductLine = new ProductLine();
+
+            vProductLine.setDescription(pProductLine.getDescription());
+            vProductLine.setProductLine(pProductLine.getProductLine());
+            vProductLine.setCreatedDate(new Date());
+
+            vProductLine.setCreatedDate(new Date());
+
+            ProductLine vProductLineSave = gProductLineRepository.save(vProductLine);
+            return new ResponseEntity<>(vProductLineSave, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return ResponseEntity.unprocessableEntity()
+                    .body("Failed to Create specified ProductLine: " + e.getCause().getCause().getMessage());
+        }
+
+    }
+
+    // get customer by id
+    @GetMapping("/customers/{customerId}")
+    public ResponseEntity<Object> getProductLineById(
+            @PathVariable Integer customerId) {
+        Optional<ProductLine> vProductLineData = gProductLineRepository.findById(customerId);
         if (vProductLineData.isPresent()) {
             try {
                 ProductLine vProductLine = vProductLineData.get();
@@ -73,70 +93,40 @@ public class ProductLineController {
         }
     }
 
-    // create new payment
-    @PostMapping("customers/{customerId}/product-lines")
-    public ResponseEntity<Object> createNewProductLine(@Valid @RequestBody ProductLine pProductLine,
-            @PathVariable Integer customerId) {
-        Optional<Customer> vCustomerData = gCustomerRepository.findById(customerId);
-        if (vCustomerData.isPresent()) {
+    // Update customer by id
+    @PutMapping("/customers/{id}")
+    public ResponseEntity<Object> updateProductLine(
+            @PathVariable Integer id,
+            @Valid @RequestBody ProductLine pProductLine) {
+        Optional<ProductLine> vProductLineData = gProductLineRepository.findById(id);
+        if (vProductLineData.isPresent()) {
             try {
-                ProductLine vProductLine = new ProductLine();
-                vProductLine.setCustomer(vCustomerData.get());
-                vProductLine.setAmount(pProductLine.getAmount());
-                vProductLine.setCheckNumber(pProductLine.getCheckNumber());
-                vProductLine.setProductLineDate(pProductLine.getProductLineDate());
-                // save payment & return
-                ProductLine vSavedProductLine = gProductLineRepository.save(vProductLine);
-                return new ResponseEntity<>(vSavedProductLine, HttpStatus.CREATED);
+                ProductLine vProductLine = vProductLineData.get();
+
+                vProductLine.setDescription(pProductLine.getDescription());
+                vProductLine.setProductLine(pProductLine.getProductLine());
+                vProductLine.setUpdatedDate(new Date());
+
+                ProductLine vProductLineSave = gProductLineRepository.save(vProductLine);
+                return new ResponseEntity<>(vProductLineSave, HttpStatus.OK);
             } catch (Exception e) {
                 return ResponseEntity.unprocessableEntity()
-                        .body("Failed to Create specified ProductLine: " + e.getCause().getCause().getMessage());
+                        .body("Failed to Update specified ProductLine: " + e.getCause().getCause().getMessage());
             }
         } else {
-            Customer vCustomerNull = new Customer();
-            return new ResponseEntity<>(vCustomerNull, HttpStatus.NOT_FOUND);
-        }
-
-    }
-
-    // Update payment by id
-    @PutMapping("customers/{customerId}/product-lines/{id}")
-    public ResponseEntity<Object> updateProductLine(
-            @PathVariable Integer customerId,
-            @PathVariable Integer paymentId,
-            @Valid @RequestBody ProductLine pProductLine) {
-        Optional<ProductLine> vProductLineData = gProductLineRepository.findById(paymentId);
-        if (vProductLineData.isPresent()) {
-            Optional<Customer> vCustomerData = gCustomerRepository.findById(customerId);
-            if (vCustomerData.isPresent()) {
-                try {
-                    ProductLine vProductLine = vProductLineData.get();
-                    vProductLine.setAmount(pProductLine.getAmount());
-                    vProductLine.setCheckNumber(pProductLine.getCheckNumber());
-                    vProductLine.setProductLineDate(pProductLine.getProductLineDate());
-                    ProductLine vSavedProductLine = gProductLineRepository.save(vProductLine);
-                    return new ResponseEntity<>(vSavedProductLine, HttpStatus.OK);
-                } catch (Exception e) {
-                    return ResponseEntity.unprocessableEntity()
-                            .body("Failed to Update specified Customer: " + e.getCause().getCause().getMessage());
-                }
-            } else {
-                ProductLine vProductLineNull = new ProductLine();
-                return new ResponseEntity<>(vProductLineNull, HttpStatus.NOT_FOUND);
-            }
-        } else {
-            Customer vCustomerNull = new Customer();
-            return new ResponseEntity<>(vCustomerNull, HttpStatus.NOT_FOUND);
+            ProductLine vProductLineNull = new ProductLine();
+            return new ResponseEntity<>(vProductLineNull, HttpStatus.NOT_FOUND);
         }
     }
 
-    // Delete payment by id
-    @DeleteMapping("/product-lines/{paymentId}")
-    private ResponseEntity<Object> deleteProductLineById(@PathVariable Integer paymentId) {
-        Optional<ProductLine> vProductLineData = gProductLineRepository.findById(paymentId);
+    // Delete customer by id
+    @DeleteMapping("/customers/{id}")
+    private ResponseEntity<Object> deleteProductLineById(
+            @PathVariable Integer id) {
+        Optional<ProductLine> vProductLineData = gProductLineRepository.findById(id);
         if (vProductLineData.isPresent()) {
             try {
-                gProductLineRepository.deleteById(paymentId);
+                gProductLineRepository.deleteById(id);
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             } catch (Exception e) {
                 return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
--- src/main/java/com/api/pizza/model/ProductLine.java
+++ src/main/java/com/api/pizza/model/ProductLine.java
@@ -1,6 +1,6 @@
 package com.api.pizza.model;
 
-import java.sql.Date;
+import java.util.Date;
 
 import javax.persistence.*;
 
