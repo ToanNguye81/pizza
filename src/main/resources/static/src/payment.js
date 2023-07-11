bc74e337263cc9f77d1ba2d1a606439d96ea98da
Your Name <n.t.quoctoan@gmail.com>
2023-07-11 20:27:45 +0700

    edit payment.js

--- src/main/resources/static/src/payment.js
+++ src/main/resources/static/src/payment.js
@@ -1,11 +1,11 @@
 let gCustomerId = 0;
-let gOrderId = 0;
+let gPaymentId = 0;
 // customer
 $.get(`/customers`, loadCustomerToSelect);
-$.get(`/payments`, loadOrderToTable);
+$.get(`/payments`, loadPaymentToTable);
 let customerSelectElement = $("#select-customer");
-function loadCustomerToSelect(paramCustomer) {
-  paramCustomer.forEach((customer) => {
+function loadCustomerToSelect(pCustomer) {
+  pCustomer.forEach((customer) => {
     $("<option>", {
       text: customer.firstName + " " + customer.lastName,
       value: customer.id,
@@ -16,156 +16,10 @@ customerSelectElement.change(onGetCustomerChange);
 function onGetCustomerChange(event) {
   gCustomerId = event.target.value;
   if (gCustomerId == 0) {
-    $.get(`/payments`, loadOrderToTable);
+    $.get(`/payments`, loadPaymentToTable);
   } else {
-    $.get(`/customers/${gCustomerId}/payments`, loadOrderToTable);
+    $.get(`/customers/${gCustomerId}/payments`, loadPaymentToTable);
   }
-}
-
-// let customer = {
-//   newCustomer: {
-//     fullName: "",
-//     email: "",
-//     phone: "",
-//     address: "",
-//   },
-//   onCreateNewCustomerClick() {
-//     gCustomerId = 0;
-//     $("#modal-create-customer").modal("show");
-//   },
-//   onUpdateCustomerClick() {
-//     if (gCustomerId != 0) {
-//       $("#modal-create-customer").modal("show");
-//       $.get(`/customers/${gCustomerId}`, loadCustomerToInput);
-//     } else {
-//       alert("Please select a customer to update");
-//     }
-//   },
-//   onSaveCustomerClick() {
-//     this.newCustomer = {
-//       fullName: $("#inp-fullName").val().trim(),
-//       email: $("#inp-email").val().trim(),
-//       phone: $("#inp-phone").val().trim(),
-//       address: $("#inp-address").val().trim(),
-//     };
-//     if (gCustomerId == 0) {
-//       if (validateCustomer(this.newCustomer)) {
-//         $.ajax({
-//           url: `/customers`,
-//           method: "POST",
-//           contentType: "application/json",
-//           data: JSON.stringify(this.newCustomer),
-//           success: () => {
-//             alert("successfully create new customer");
-//             location.reload();
-//           },
-//           error: (err) => alert(err.responseText),
-//         });
-//       }
-//     } else {
-//       if (validateCustomer(this.newCustomer)) {
-//         $.ajax({
-//           url: `/customers/${gCustomerId}`,
-//           method: "PUT",
-//           contentType: "application/json",
-//           data: JSON.stringify(this.newCustomer),
-//           success: () => {
-//             alert("successfully update customer with id: " + gCustomerId);
-//             location.reload();
-//           },
-//           error: (err) => alert(err.responseText),
-//         });
-//       }
-//     }
-//   },
-//   onDeleteCustomerClick() {
-//     if (gCustomerId != 0) {
-//       $("#modal-delete-customer").modal("show");
-//     } else {
-//       alert("Please select a customer to delete");
-//     }
-//   },
-//   onDeleteAllCustomerClick() {
-//     gCustomerId = 0;
-//     $("#modal-delete-customer").modal("show");
-//   },
-//   onConfirmDeleteCustomerClick() {
-//     if (gCustomerId != 0) {
-//       $.ajax({
-//         url: `/customers/${gCustomerId}`,
-//         method: "delete",
-//         success: () => {
-//           alert("successfully delete customer with id:" + gCustomerId);
-//           location.reload();
-//         },
-//         error: (err) => alert(err.responseText),
-//       });
-//     } else {
-//       $.ajax({
-//         url: `/customers`,
-//         method: "delete",
-//         success: () => {
-//           alert("successfully delete all customers");
-//           location.reload();
-//         },
-//         error: (err) => alert(err.responseText),
-//       });
-//     }
-//   },
-// };
-
-// $("#btn-create-customer").click(customer.onCreateNewCustomerClick);
-// $("#btn-update-customer").click(customer.onUpdateCustomerClick);
-// $("#btn-save-customer").click(customer.onSaveCustomerClick);
-// $("#btn-delete-customer").click(customer.onDeleteCustomerClick);
-// $("#btn-delete-all-customer").click(customer.onDeleteAllCustomerClick);
-// $("#btn-confirm-delete-customer").click(customer.onConfirmDeleteCustomerClick);
-
-// function loadCustomerToInput(paramCustomer) {
-//   $("#inp-fullName").val(paramCustomer.fullName);
-//   $("#inp-email").val(paramCustomer.email);
-//   $("#inp-phone").val(paramCustomer.phone);
-//   $("#inp-address").val(paramCustomer.address);
-// }
-
-// function validateCustomer(paramCustomer) {
-//   let vResult = true;
-//   try {
-//     if (paramCustomer.fullName == "") {
-//       vResult = false;
-//       throw `full name can't be empty`;
-//     }
-
-//     if (paramCustomer.email == "") {
-//       vResult = false;
-//       throw ` email can't be empty`;
-//     }
-//     if (!validateEmail(paramCustomer.email)) {
-//       vResult = false;
-//       throw `must need right email`;
-//     }
-//     if (paramCustomer.phone == "") {
-//       vResult = false;
-//       throw `phone can't be empty`;
-//     }
-//     if (paramCustomer.phone.length < 10 || isNaN(paramCustomer.phone)) {
-//       vResult = false;
-//       throw `Cần nhập đúng kiểu số điện thoại phải không có chữ cái và đúng 10 số`;
-//     }
-//     if (paramCustomer.address == "") {
-//       vResult = false;
-//       throw `Address can't be empty`;
-//     }
-//   } catch (e) {
-//     alert(e);
-//   }
-//   return vResult;
-// }
-
-function validateEmail(email) {
-  const re =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
 }
 // payment
 let paymentTable = $("#payment-table").DataTable({
@@ -185,68 +39,69 @@ let paymentTable = $("#payment-table").DataTable({
   ],
 });
 
-function loadOrderToTable(paramOrder) {
+function loadPaymentToTable(pPayment) {
   paymentTable.clear();
-  paymentTable.rows.add(paramOrder);
+  paymentTable.rows.add(pPayment);
   paymentTable.draw();
 }
 
 let payment = {
-  newOrder: {
+  newPayment: {
     checkNumber: "",
     paymentDate: "",
     amount: "",
   },
-  onNewOrderClick() {
-    gOrderId = 0;
-    this.newOrder = {
+  onNewPaymentClick() {
+    gPaymentId = 0;
+    this.newPayment = {
       checkNumber: $("#inp-check-number").val().trim(),
       paymentDate: $("#inp-payment-date").val().trim(),
       amount: $("#inp-amount").val().trim(),
     };
-    if (validateOrder(this.newOrder)) {
+    console.log(this.newPayment.paymentDate);
+    if (validatePayment(this.newPayment)) {
       if (gCustomerId == 0) {
         alert(`Please select customer to create a new payment`);
       } else {
         $.ajax({
           url: `/customers/${gCustomerId}/payments`,
           method: "POST",
-          data: JSON.stringify(this.newOrder),
+          data: JSON.stringify(this.newPayment),
           contentType: "application/json",
           success: () => {
-            alert(`Order created successfully`);
-            $.get(`/customers/${gCustomerId}/payments`, loadOrderToTable);
-            resetOrderInput();
+            alert(`Payment created successfully`);
+            $.get(`/customers/${gCustomerId}/payments`, loadPaymentToTable);
+            resetPaymentInput();
           },
         });
       }
     }
   },
-  onEditOrderClick() {
+  onEditPaymentClick() {
     vSelectedRow = $(this).parents("tr");
     vSelectedData = paymentTable.row(vSelectedRow).data();
-    gOrderId = vSelectedData.id;
-    $.get(`/payments/${gOrderId}`, loadOrderToInput);
+    gPaymentId = vSelectedData.id;
+    $.get(`/payments/${gPaymentId}`, loadPaymentToInput);
   },
-  onUpdateOrderClick() {
-    this.newOrder = {
+  onUpdatePaymentClick() {
+    this.newPayment = {
       checkNumber: $("#inp-check-number").val().trim(),
       paymentDate: $("#inp-payment-date").val().trim(),
       amount: $("#inp-amount").val().trim(),
     };
-    if (validateOrder(this.newOrder)) {
+    if (validatePayment(this.newPayment)) {
       if (gCustomerId == 0) {
         alert(`Please select customer to update a new payment`);
       } else {
         $.ajax({
-          url: `/payments/${gOrderId}`,
+          url: `/payments/${gPaymentId}`,
           method: "PUT",
-          data: JSON.stringify(this.newOrder),
+          data: JSON.stringify(this.newPayment),
           contentType: "application/json",
           success: () => {
-            alert(`Order updated successfully`);
-            $.get(`/customers/${gCustomerId}/payments`, loadOrderToTable);
-            resetOrderInput();
+            alert(`Payment updated successfully`);
+            $.get(`/customers/${gCustomerId}/payments`, loadPaymentToTable);
+            resetPaymentInput();
           },
         });
       }
@@ -256,36 +111,36 @@ let payment = {
     $("#modal-delete-payment").modal("show");
     vSelectedRow = $(this).parents("tr");
     vSelectedData = paymentTable.row(vSelectedRow).data();
-    gOrderId = vSelectedData.id;
+    gPaymentId = vSelectedData.id;
   },
-  onDeleteAllOrderClick() {
+  onDeleteAllPaymentClick() {
     $("#modal-delete-payment").modal("show");
-    gOrderId = 0;
+    gPaymentId = 0;
   },
-  onOrderConfirmDeleteClick() {
-    if (gOrderId == 0) {
+  onPaymentConfirmDeleteClick() {
+    if (gPaymentId == 0) {
       $.ajax({
         url: `/payments`,
-        method: "delete",
+        method: "DELETE",
         success: () => {
-          alert("All Order was successfully deleted");
-          // $.get(`/payments`, loadOrderToTable);
+          alert("All Payment was successfully deleted");
+          // $.get(`/payments`, loadPaymentToTable);
           gCustomerId == 0
-            ? $.get(`/payments`, loadOrderToTable)
-            : $.get(`customers/${gCustomerId}/payments`, loadOrderToTable);
+            ? $.get(`/payments`, loadPaymentToTable)
+            : $.get(`customers/${gCustomerId}/payments`, loadPaymentToTable);
           $("#modal-delete-payment").modal("hide");
         },
         error: (err) => alert(err.responseText),
       });
     } else {
       $.ajax({
-        url: `/payments/${gOrderId}`,
-        method: "delete",
+        url: `/payments/${gPaymentId}`,
+        method: "DELETE",
         success: () => {
-          alert(`Order with id ${gOrderId} was successfully deleted`);
+          alert(`Payment with id ${gPaymentId} was successfully deleted`);
           gCustomerId == 0
-            ? $.get(`/payments`, loadOrderToTable)
-            : $.get(`customers/${gCustomerId}/payments`, loadOrderToTable);
+            ? $.get(`/payments`, loadPaymentToTable)
+            : $.get(`customers/${gCustomerId}/payments`, loadPaymentToTable);
 
           $("#modal-delete-payment").modal("hide");
         },
@@ -295,25 +150,25 @@ let payment = {
   },
 };
 
-$("#create-payment").click(payment.onNewOrderClick);
-$("#update-payment").click(payment.onUpdateOrderClick);
-$("#delete-all-payment").click(payment.onDeleteAllOrderClick);
-$("#btn-confirm-delete-payment").click(payment.onOrderConfirmDeleteClick);
-$("#payment-table").on("click", ".fa-edit", payment.onEditOrderClick);
+$("#create-payment").click(payment.onNewPaymentClick);
+$("#update-payment").click(payment.onUpdatePaymentClick);
+$("#delete-all-payment").click(payment.onDeleteAllPaymentClick);
+$("#btn-confirm-delete-payment").click(payment.onPaymentConfirmDeleteClick);
+$("#payment-table").on("click", ".fa-edit", payment.onEditPaymentClick);
 $("#payment-table").on("click", ".fa-trash", payment.onDeleteCustomerByIdClick);
 
-function validateOrder(paramOrder) {
+function validatePayment(pPayment) {
   let vResult = true;
   try {
-    if (paramOrder.checkNumber == "") {
+    if (pPayment.checkNumber == "") {
       vResult = false;
       throw `100. Check number can't empty`;
     }
-    if (paramOrder.paymentDate == "") {
+    if (pPayment.paymentDate == "") {
       vResult = false;
       throw `200. Payment date can't empty`;
     }
-    if (paramOrder.amount == "") {
+    if (pPayment.amount == "") {
       vResult = false;
       throw `300. Amount can't empty`;
     }
@@ -323,13 +178,13 @@ function validateOrder(paramOrder) {
   return vResult;
 }
 
-function loadOrderToInput(paramOrder) {
-  $("#inp-check-number").val(paramOrder.checkNumber);
-  $("#inp-payment-date").val(paramOrder.paymentDate);
-  $("#inp-amount").val(paramOrder.amount);
+function loadPaymentToInput(pPayment) {
+  $("#inp-check-number").val(pPayment.checkNumber);
+  $("#inp-payment-date").val(pPayment.paymentDate);
+  $("#inp-amount").val(pPayment.amount);
 }
 
-function resetOrderInput() {
+function resetPaymentInput() {
   $("#inp-check-number").val("");
   $("#inp-payment-date").val("");
   $("#inp-amount").val("");
