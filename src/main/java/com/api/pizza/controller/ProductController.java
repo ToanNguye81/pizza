192f8fc5499bd51c99f77a866c0e183c96759958
Your Name <n.t.quoctoan@gmail.com>
2023-07-15 16:28:05 +0700

    edit CRUD Product

--- src/main/java/com/api/pizza/controller/ProductController.java
+++ src/main/java/com/api/pizza/controller/ProductController.java
@@ -24,7 +24,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.pizza.entity.Product;
-import com.api.pizza.entity.Product;
 import com.api.pizza.entity.ProductLine;
 import com.api.pizza.repository.IProductLineRepository;
 import com.api.pizza.repository.IProductRepository;
