bc1a990a95f26355ebd0dea9895135463c3b4c8a
Your Name <n.t.quoctoan@gmail.com>
2023-07-24 17:12:25 +0700

    update findAllByCountryIn customer

--- src/main/java/com/api/pizza/repository/ICustomerRepository.java
+++ src/main/java/com/api/pizza/repository/ICustomerRepository.java
@@ -1,9 +1,15 @@
 package com.api.pizza.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.api.pizza.entity.Customer;
 
 public interface ICustomerRepository extends JpaRepository<Customer, Integer> {
+
+    Page<Customer> findAllByCountryIn(List<String> countryList, Pageable pageable);
 
 }
